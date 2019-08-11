907e39db0ccef8946461fc9c1c79cc97e738365b
differui <differui@gmail.com>
2019-08-11 14:45:43 +0800

    :sparkles: create .editorconfig

--- happen.sh
+++ happen.sh
@@ -5,8 +5,7 @@ mkdir -p $1
 mkdir -p $1/src
 
 # create templates
-INDEX_TS="console.log('App is ruuning...')
-"
+INDEX_TS="console.log('App is ruuning...')"
 INDEX_HTML='<!DOCTYPE html>
 <html lang="en">
   <head>
@@ -19,14 +18,15 @@ INDEX_HTML='<!DOCTYPE html>
     <div id="root"></div>
     <script src="./src/index.ts"></script>
   </body>
-</html>
-'
+</html>'
+EDITORCONFIG='[*.{js,tx,tsx,html,css,sass,scss,less,styl}]
+  indent_style = space
+  indent_size = 2'
 PRETTIERRC='{
   "trailingComma": "es5",
   "tabWidth": 2,
   "singleQuote": true
-}
-'
+}'
 TSCONFIG='{
   "compilerOptions": {
     "jsx": "react",
@@ -35,13 +35,13 @@ TSCONFIG='{
     "noImplicitAny": true
   },
   "include": ["src/*.tsx"]
-}
-'
+}'
 
 # create files
 echo "$INDEX_TS" > $1/src/index.ts
 echo "$INDEX_HTML" > $1/index.html
 echo "$PRETTIERRC" > $1/.prettierrc
+echo "$EDITORCONFIG" > $1/.editorconfig
 echo "$TSCONFIG" > $1/tsconfig.json
 
 echo "MAKE $1 HAPPEN!"
